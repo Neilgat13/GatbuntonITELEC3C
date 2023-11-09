1860614eaa7d93c36e051dbc371f5ad9309d1114
Neilgat13 <143276347+Neilgat13@users.noreply.github.com>
2023-11-09 19:54:07 +0800

    11/9/2023 commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,28 +7,33 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            
-            <div class="container">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">ID</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Created At</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($users as $user)
-                            <tr>
-                                <th scope="row">{{ $user->id }}</th>
-                                <td>{{ $user->name }}</td>
-                                <td>{{ $user->email }}</td>
-                                <td>{{ $user->created_at }}</td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Created At</th>
+    </tr>
+  </thead>
+  
+  <tbody>
+
+    @foreach ($users as $user)
+    <tr>
+      <th scope="row">1</th>
+
+      <td>{{$user->name}}
+
+      </td>
+      <td>{{$user->email}}</td>
+      <td>{{$user->created_at}}</td>
+    </tr>
+
+    @endforeach
+  </tbody>
+</table>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,16 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
-
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
@@ -22,12 +21,20 @@ Route::get('/', function () {
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
-    'verified',
+    'verified'
 ])->group(function () {
     Route::get('/dashboard', function () {
+        
         $users = User::all();
+
         return view('dashboard', compact('users'));
+
     })->name('dashboard');
 });
 
-Route::get('/all/category',[CategoryController::class, 'index'])->name('AllCat');
+Route::get('/all/category', [CategoryController::class, 'index'])->name('AllCat');
+Route::post('/add_category',[CategoryController::class, 'create']);
+Route::get('/edit_category/{id}', [CategoryController::class, 'edit']);
+Route::post('/edit_category_confirm/{id}',[CategoryController::class, 'edit_confirm']);
+Route::get('/delete_category/{id}', [CategoryController::class, 'delete']);
+
